6fb04c317be1351a15d52edd25e281123124acba
Sana Nqoba Mngadi <sana.mngadi@gmail.com>
2019-10-07 22:59:21 +0200

    Full Rebuild of WebApp

--- chatter/README.md
+++ chatter/README.md
@@ -2,17 +2,12 @@
 
 This Project allows users to chat in channels using Javascript and Python 3.5+
 
-## Home Page
-In this page javascript will check if the user is using a username, if not it will prompt to enter a username to use.
-User can see all the available channels on the server, with a refresh button to refresh the list.
-Users will also have a recently visited list to see the channels they've visited in the past.
-If a user needs to make a new channel, they can easily create a new one by clicking on the +Create Channel button and name it anything, as long as it doesn't conflict with other channel names.
-From this page users can join any channel.
+## Index/Home Page
+You can create new channels, send live real time messages and see other available channels and see previous messages up to 100.
+Channel names can only be letters, the names can only be a minimum of 1 and a maximun of 8.
 
-## Channel page
-Users can chat with each other and ask the bot things (The bot is very very very simple).
-The javascript will remember that the user is in the channel so if the tab is closed and they return it will send them to the channel they were still in.
-The chat will be logged up to 100 messages on the server so when new users and previous users return they can see what has been going on in the channel.
+## Login Page
+Here you will have to provide a username to use when messaging.
 
 ## How to setup
 0. `pip install -r requirements.txt`
--- chatter/application.py
+++ chatter/application.py
@@ -12,32 +12,29 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-system_data = {"channel_names": []}
+system_data = {"channel_names": ['General']}
 channel_storage = []
 
 # Prevent Caching so CSS changes can take immediate effect
 @app.after_request
-def after_request(respose):
-	respose.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-	respose.headers["Expire"] = 0
-	respose.headers["Pragma"] = "no-cache"
-	return respose
+def after_request(response):
+	response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	response.headers["Expire"] = 0
+	response.headers["Pragma"] = "no-cache"
+	response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+	return response
 
 
 @app.route("/")
 def index():
 	"""Show chatters channels"""
-	return render_template("index.html", channel_list=system_data["channel_names"])
+
+	return redirect("/c/General")
 
 
-@app.route("/channel_name/<string:channel>", methods=["GET", "POST"])
+@app.route("/c/<string:channel>", methods=["GET", "POST"])
 def channel_name(channel):
 	"""Load the apropriate channel page"""
-
-	# Check if the channel exist in the available channel list
-	if channel not in system_data["channel_names"]:
-		print("Channel doesn't exist!")
-		return redirect("/")
 
 	# Store the Data to send to the HTML code in this variable
 	channel_stored_chat_HTML = None
@@ -51,23 +48,28 @@ def channel_name(channel):
 
 		# Check to see if any of the dictionary list matches the channel being accesssed
 		for alpha in range(len(channel_storage)):
-			if channel in channel_storage[alpha]:
+			if "/c/"+channel in channel_storage[alpha]:
 				channel_present = True
 				channel_present_n = alpha
 
 		# If the channel chat log exist in the logs add the chat log to the variable
 		if channel_present:
-			channel_stored_chat_HTML = channel_storage[channel_present_n][channel]
+			# Check if the user is requesting to go to a channel else just send them to the General channel
+			if channel:
+				channel_stored_chat_HTML = channel_storage[channel_present_n]["/c/"+channel]
+			else:
+				channel_stored_chat_HTML = channel_storage[channel_present_n]["General"]
 
-	return render_template("channel.html", channel_name=channel,
+	return render_template("index.html", channel_name=channel,
+										   channel_list=system_data["channel_names"],
 										   channel_stored_chat_HTML=channel_stored_chat_HTML)
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
 	""" Login """
 
-	return redirect("/")
+	return render_template("login.html")
 
 
 @app.route("/logout")
@@ -84,18 +86,21 @@ def refresh_channels():
 	return jsonify({"channel_list": system_data["channel_names"]})
 
 
-@app.route("/create_channel", methods=["POST", "GET"])
-def create_channel():
+@app.route("/c/<string:channel>/create_channel", methods=["POST", "GET"])
+def create_channel(channel):
 	"""Create a new channel"""
 
 	creation_status = None
 	new_channel = str(request.form.get("new_channel_name"))
 
-	if new_channel not in system_data["channel_names"]:
-		system_data["channel_names"].append(new_channel)
-		creation_status = "GOOD"
+	if new_channel.isalpha():
+		if new_channel not in system_data["channel_names"]:
+			system_data["channel_names"].append(new_channel)
+			creation_status = "GOOD"
+		else:
+			creation_status = "ALREADY"
 	else:
-		creation_status = "ALREADY"
+		creation_status = "BAD_CHAR"
 
 	return jsonify({"CREATE_STATUS": creation_status})
 
@@ -138,7 +143,7 @@ def message_sender(data):
 
 	# Check if the BOT is being activated
 	if len(data["msg"]) >= 4:
-		if str(data["msg"][:4]) == "@BOT":
+		if str(data["msg"][:4]).lower() == "@bot":
 			bot(data)
 
 
